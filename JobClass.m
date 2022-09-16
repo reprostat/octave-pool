7a563b1848c6f06918255636be36a56999866e52
tiborauer <tibor.auer@gmail.com>
2022-09-16 10:22:39 +0200

    ADD - Job

---
+++ JobClass.m
@@ -0,0 +1,88 @@
+classdef JobClass < handle
+    properties
+        ID
+        Name
+        AdditionalPaths = {}
+        Tasks = TaskClass.empty;
+    endproperties
+
+    properties (Dependent)
+        State
+    endproperties
+
+    properties (Hidden)
+        Pool
+        Folder
+        latestTaskID = 0
+    endproperties
+
+    properties (Hidden, Access = protected)
+        schedulerID = NaN
+    endproperties
+
+    methods
+        function this = JobClass(Pool)
+            this.Pool = Pool;
+
+            this.ID = this.Pool.latestJobID+1;
+            this.Name = sprintf('Job%d',this.ID);
+
+            this.Folder = fullfile(this.Pool.JobStorageLocation,this.Name);
+%             while exist(this.Folder,'dir')
+%                 this.Folder = [this.Folder '+'];
+%             end
+            mkdir(this.Folder);
+        endfunction
+
+        function val = isvalid(this)
+            val = exist(this.Folder,'dir');
+        endfunction
+
+        function delete(this)
+            if ~this.isvalid, return; endif
+            if this.cancel
+                confirm_recursive_rmdir(0,'local')
+                rmdir(this.Folder,'s');
+                delete@handle(this)
+            else
+                warning('Job %s could not be killed!\nYou may need to kill manually by calling %s.',this.ID,this.Pool.getJobDeleteStringFcn(this.schedulerID));
+            endif
+        endfunction
+
+        function s = cancel(this)
+            s = false;
+            if ~any(strcmp({'unknown','finished','error'},this.State)), [s, w] = system(this.Pool.getJobDeleteStringFcn(this.schedulerID)); endif
+            if s, warning(w);
+%             else
+%                 this.Pool.Jobs([this.Pool.Jobs.ID]==this.ID) = [];
+            endif
+            s = ~s;
+        endfunction
+
+        function addTask(this,Name,varargin)
+            Task = TaskClass(this,Name,varargin{:});
+            this.Tasks(end+1) = Task;
+            this.latestTaskID = this.latestTaskID + 1;
+        endfunction
+
+        function Submit(this)
+            [s, w] = system(this.Pool.getSubmitStringFcn(this));
+            for t = 1:numel(this.Tasks)
+                this.Tasks(t).StartDateTime = datetime();
+            endfor
+            if ~s
+                this.schedulerID = this.Pool.getSchedulerIDFcn(w);
+            endif
+        endfunction
+
+        function val = get.State(this)
+            val = 'unknown';
+            if ~isnan(this.schedulerID)
+                val = this.Pool.getJobStateFcn(this.schedulerID);
+            endif
+            if any(strcmp({'finished','error'},val)), val = this.Tasks.State; endif
+        endfunction
+    endmethods
+
+endclassdef
+
