c25a0f203e381885a65979ea49e63ff006b9f664
tiborauer <tibor.auer@gmail.com>
2022-09-20 09:46:44 +0100

    ENH - worker logging

--- WorkerClass.m
+++ WorkerClass.m
@@ -1,19 +1,42 @@
-classdef WorkerClass
+classdef workerClass < handle
     properties
-        Host
-        ProcessId
+        host
+        processID
+        logFile
     endproperties
 
     methods
-        function this = WorkerClass(host='localhost',pid=0)
-            this.Host = host;
-            this.ProcessId = pid;
+        function this = workerClass(host='',pid=[],logFile='')
+            this.host = host;
+            this.processID = pid;
+            this.logFile = logFile;
         endfunction
+
+        function delete(this)
+            delete(this.logFile);
+        endfunction
+
+        function set.logFile(this,val)
+            if ~strcmp(this.logFile,val)
+                if ~isempty(this.logFile), movefile(this.logFile,val);
+                else, fclose(fopen(val,'w'));
+                endif
+                this.logFile = val;
+            endif
+        endfunction
+
+        function addLog(this,msg)
+            d = datetime;
+            fid = fopen(this.logFile,'a');
+            fprintf(fid, '[%s] - %s\n',d.toString,msg);
+            fclose(fid);
+        endfunction
+
     endmethods
 
     methods  (Static = true)
         function this = empty()
-            this = WorkerClass();
+            this = workerClass();
             this = this(false);
         endfunction
     endmethods
