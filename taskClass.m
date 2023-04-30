e762c538f4a70e36f1daf869347f0a45bbee735f
tiborauer <tibor.auer@gmail.com>
2023-04-30 09:23:17 +0100

    FIX - special arguments for tasks

--- taskClass.m
+++ taskClass.m
@@ -12,6 +12,7 @@ classdef taskClass < handle
     properties (Hidden)
         folder
         shellFile
+        logFile % system StdOut
     end
 
     properties (Access = private)
@@ -48,20 +49,22 @@ classdef taskClass < handle
             end
             this._worker = workerClass(fullfile(this.folder,'log.txt'));
             this.scriptFile = fullfile(this.folder,'run.m');
+            this.logFile = fullfile(this.folder,'std.out');
             this.diaryFile = fullfile(this.folder,'diary.txt');
             this.processFile = fullfile(this.folder,'process');
             this.errorFile = fullfile(this.folder,'error.mat');
             if nOut > 0, this.outFile = fullfile(this.folder,'out.mat'); end
 
-            ## assemble command
+            % assemble command
             pathCommand = '';
             varCommand = '';
             command = func2str(func);
             userVariable = [];
-            ## - execute
+            % - execute
             if ~isempty(args)
-                ## - parse special cases
-                args{cellfun(@(a) ischar(a) & strcmp(a,'$thisworker'), args)} = struct(this._worker);
+                % - parse special cases
+                argWorker = cellfun(@(a) ischar(a) & strcmp(a,'$thisworker'), args);
+                if any(argWorker), args{argWorker} = struct(this._worker); end
 
                 this.inputArguments = args;
                 varStr = sprintf('arg%d,',1:numel(this.inputArguments));
@@ -71,24 +74,24 @@ classdef taskClass < handle
                 command = [command, varStr];
                 command(end) = ')';
             end
-            ## - output
+            % - output
             if nOut > 0
                 outputStr = strjoin(arrayfun(@(o) sprintf('o%d',o), 1:nOut, 'UniformOutput', false),',');
                 command = sprintf('[ %s ] = %s; save(''-binary'',''%s'', ''%s'')', outputStr, command, this.outFile, strrep(outputStr,',',''','''));
             end
-            ## - path
+            % - path
             if ~isempty(this.parent.additionalPaths)
                 userVariable.reqpath = this.parent.additionalPaths;
                 pathCommand = sprintf('load(''%s'',''reqpath'');addpath(reqpath{:});',fullfile(this.folder,'data.mat'));
             end
-            ## - input
+            % - input
             if ~isempty(userVariable)
                 save('-binary',fullfile(this.folder,'data.mat'),'-struct','userVariable');
                 varCommand = sprintf('load(''%s'',%s);',fullfile(this.folder,'data.mat'),['''' strrep(varStr(1:end-1),',',''',''') '''']);
             end
             command = [pathCommand, varCommand, command ';'];
 
-            ## create script
+            % create script
             fid = fopen(this.shellFile,'w');
             switch this.parent.pool.type
                 case 'Slurm'
@@ -98,7 +101,7 @@ classdef taskClass < handle
                     end
             end
             if ~isempty(this.parent.pool.initialConfiguration), fprintf(fid,'%s;',this.parent.pool.initialConfiguration); end
-            fprintf(fid,'%s %s', fullfile(OCTAVE_HOME, 'bin', 'octave-cli'), this.scriptFile);
+            fprintf(fid,'%s %s', fullfile(OCTAVE_HOME, 'bin', 'octave'), this.scriptFile);
             fclose(fid);
             fid = fopen(this.scriptFile,'w');
             fprintf(fid,'diary %s;\nfid = fopen(''%s'',''w''); fprintf(fid,''%%d@%%s\\n'',getpid,gethostname); fclose(fid);\ntry\n    %s\ncatch E\n    save(''-binary'',''%s'',''E'');\nend\nfid = fopen(''%s'',''a''); fprintf(fid,''%%s'',char(datetime())); fclose(fid);\ndiary off',...
